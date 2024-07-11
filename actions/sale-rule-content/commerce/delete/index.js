3c2e993057ade50f1b817f85fa67dd0d0de63eca
Kumar Eayshwary <keayshwary@Kumars-MacBook-Pro.local>
2024-07-11 17:04:49 +0530

    LDHUB-54518 delete xl row

--- actions/sale-rule-content/commerce/delete/index.js
+++ actions/sale-rule-content/commerce/delete/index.js
@@ -34,18 +34,16 @@ async function main (params) {
         if (validationResult.success === false) {
             return actionErrorResponse(HTTP_BAD_REQUEST, validationResult.message)
         }
-        const oldWebsiteCodes = dataObject.pre_website.split(',');
-        const newWebsiteCodes = dataObject.post_website.split(',');
+        const websiteCodes = dataObject.website.split(',');
         const brandCode = dataObject.brand;
-        let removeFromWebsites = oldWebsiteCodes.filter(x => !newWebsiteCodes.includes(x));
-        if (removeFromWebsites.length === 0 && newWebsiteCodes.length === 0) {
+        if (websiteCodes.length === 0 && websiteCodes.length === 0) {
             return actionSuccessResponse("No changes to update")
         }
         const filePathPrefix = `${params.MICROSOFT_GRAPH_BASE_URL}/sites/${params.ENTRA_SITE_ID}/drive/root:/${getDirectoryPath(params, brandCode)}/`;
         const accessToken = getEntraAccessToken();
         const rowsData = [dataObject];
         //remove from sheet
-        for(let siteCode of removeFromWebsites) {
+        for(let siteCode of websiteCodes) {
             let filePathToRead = filePathPrefix + getFileNameToRead(siteCode);
             await removeFromExcel(accessToken, rowsData, filePathToRead);
         }
